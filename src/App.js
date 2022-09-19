8768dec8b9a9413fd0523b10eb41920cf4c3d4e2
Pavol Lantaj <palolantaj@gmail.com>
2022-09-19 21:19:49 +0200

    First sidebar prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'; 
 
 // rafce TAB (ES7+)
 const App = () => {
+  const activeMenu = true;
+
   return (
-    <h1 className="underline text-3xl">App</h1>
+    <div>
+      <BrowserRouter>
+        <div className ="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+            <TooltipComponent content ="Settings" position ="Top">
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{background:'blue',
+              borderRadius: '50%'}}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+            {/* if active menu = true, then show SIDEBAR, otherwise DONT SHOW SIDEBAR
+            
+            w-0 means WIDTH 0
+
+            dark: etc... means settings on DARK MODE
+            */}
+          </div>
+          {activeMenu ?( 
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ): (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              Sidebar w-0
+            </div>
+          )}
+
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
