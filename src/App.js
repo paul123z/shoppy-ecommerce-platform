55981f0df0ec0f4fe657c41c8f3c786512a34bba
Pavol Lantaj <palolantaj@gmail.com>
2022-09-20 21:04:39 +0200

    finishing setting structure

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'; 
 
-import {Navbar,Footer,Sidebar,ThemeSettings} from './components'
-import {Ecommerce,Orders,Calendar,Employees,Stacked,Pyramid,Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping,Editor} from './pages'
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import './App.css';
 
 import './App.css'
 // rafce TAB (ES7+)
@@ -33,11 +34,11 @@ const App = () => {
           </div>
           {activeMenu ?( 
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+              <Sidebar/>
             </div>
           ): (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              Sidebar
+            <Sidebar/>
             </div>
           )}
           {/* if active menu = true, then md:ml-72, otherwise flex-2 */}
@@ -48,37 +49,39 @@ const App = () => {
             : 'flex-2'}`  
           }>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
+              <Navbar/>
             </div>
           </div>
 
           <div>
             <Routes>
+              {/* Changing all at once, ALT CLICK CLICK CLICK , 'BACKSPACE {< ctrl+right, right } />' */}
+
               {/* Dashboard */}
-              <Route path='/' element= "ECommerce" />
-              <Route path='/ecommerce' element= "ECommerce" />
+              <Route path='/' element= {<Ecommerce/>} />
+              <Route path='/ecommerce' element= {<Ecommerce/>} />
               
 
               {/* Pages */}
-              <Route path='/orders' element= "Orders" />
-              <Route path='/employees' element= "Employees" />
-              <Route path='/customer' element= "Customers" />
+              <Route path='/orders' element= {<Orders/>} />
+              <Route path='/employees' element= {<Employees/>} />
+              <Route path='/customer' element= {<Customers/>} />
               
 
               {/* Apps */}
-              <Route path='/kanban' element= "Kanban" />
-              <Route path='/editor' element= "Editor" />
-              <Route path='/calendar' element= "Calendar" />
+              <Route path='/kanban' element= {<Kanban/>} />
+              <Route path='/editor' element= {<Editor/>} />
+              <Route path='/calendar' element= {<Calendar/>} />
               <Route path='/color-picker' element= "ColorPicker"/>
 
               {/* Charts */}
-              <Route path='/line' element= "Line" />
-              <Route path='/area' element= "Area" />
-              <Route path='/bar' element= "Bar" />
-              <Route path='/pie' element= "Pie" />
+              <Route path='/line' element= {<Line/>} />
+              <Route path='/area' element= {<Area/>} />
+              <Route path='/bar' element= {<Bar/>} />
+              <Route path='/pie' element= {<Pie/>} />
               <Route path='/color-mapping' element= "ColorMapping" />
-              <Route path='/pyramid' element= "Pyramid" />
-              <Route path='/stacked' element= "Stacked" />
+              <Route path='/pyramid' element= {<Pyramid/>} />
+              <Route path='/stacked' element= {<Stacked/>} />
             </Routes>
           </div>
         </div>
