af26a41f1b23ff8072fe5f2a7ffc92d298235bbc
Pavol Lantaj <palolantaj@gmail.com>
2022-10-02 18:51:37 +0200

    Editing Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => (
   <div className="mt-24">
-    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved | Jumbofy.com [by Pavol Lantaj]
+    <p className="dark:text-gray-200 text-gray-700 text-center mt-20">
+      © 2022 All Rights Reserved | Jumbofy.com [by Pavol Lantaj]
+    </p>
+    <p className="dark:text-gray-200 text-gray-700 text-center m-2">
+      Contact: pavol.lantaj@gmail.com
     </p>
   </div>
 );
